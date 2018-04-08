4b72b2c20efd7772489115796170fdf8599659d9
javier <00076015@uca.edu.sv>
2018-04-07 21:34:23 -0600

    Funcionalidad completa

--- app/src/main/java/com/example/javier/tarea02/MainActivity.java
+++ app/src/main/java/com/example/javier/tarea02/MainActivity.java
@@ -3,6 +3,10 @@ package com.example.javier.tarea02;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.drawable.Drawable;
+import android.net.Uri;
 import android.support.v4.graphics.drawable.RoundedBitmapDrawable;
 import android.support.v4.graphics.drawable.RoundedBitmapDrawableFactory;
 import android.support.v7.app.AppCompatActivity;
@@ -13,45 +17,82 @@ import android.widget.ImageView;
 import android.widget.TextView;
 
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity {
-    TextView nombre1,carrera,twitter,github,correo1,telefono;
     ImageView imagen1;
+    /*Declarando Variables*/
+    String nombres, carrera1, twitter1, github1, correoe, tel;
+    TextView nombre1, carrera, twitter, github, correo1, telefono, creador;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        imagen1 = (ImageView) findViewById(R.id.prueba);
 
         ImageView prueba = (ImageView) findViewById(R.id.prueba);
         Bitmap bitmap = BitmapFactory.decodeResource(getResources(), R.drawable.perfilv);
-        RoundedBitmapDrawable roundedBitmapDrawable = RoundedBitmapDrawableFactory.create(getResources(),bitmap);
+        RoundedBitmapDrawable roundedBitmapDrawable = RoundedBitmapDrawableFactory.create(getResources(), bitmap);
 
         roundedBitmapDrawable.setCircular(true);
         prueba.setImageDrawable(roundedBitmapDrawable);
 
-        imagen1 = findViewById(R.id.prueba);
-        nombre1 = findViewById(R.id.envia1);
-        carrera = findViewById(R.id.envia2);
-        twitter = findViewById(R.id.envia3);
-        github = findViewById(R.id.envia4);
-        correo1 = findViewById(R.id.envia5);
-        telefono = findViewById(R.id.envia6);
 
     }
 
-    public void compartir(View view){
-        Intent comparte = new Intent();
-        comparte.setAction(Intent.ACTION_SEND);
-        comparte.setType("text/plain");
-        comparte.putExtra(Intent.EXTRA_TEXT, "Nombre:"+nombre1.getText() + "\n Carrera:"+carrera.getText() + "\n"+twitter.getText() + "\n"+github.getText() + "\n"+correo1.getText() + "\n"+telefono.getText());
+    public void compartir(View view) {
+        Bitmap bitmap1 = vista(imagen1);
 
-        if(comparte.resolveActivity(getPackageManager()) !=null){
+        nombre1 = (TextView) findViewById(R.id.envia1);
+        nombres = nombre1.getText().toString();
+        carrera = (TextView) findViewById(R.id.envia2);
+        carrera1 = carrera.getText().toString();
+        twitter = (TextView) findViewById(R.id.envia3);
+        twitter1 = twitter.getText().toString();
+        github = (TextView) findViewById(R.id.envia4);
+        github1 = github.getText().toString();
+        correo1 = (TextView) findViewById(R.id.envia5);
+        correoe = correo1.getText().toString();
+        telefono = (TextView) findViewById(R.id.envia6);
+        tel = telefono.getText().toString();
+        creador = (TextView) findViewById(R.id.envia7);
 
-            startActivity(comparte);
+        try {
+            File file = new File(this.getExternalCacheDir(), "apoyo.png");
+            FileOutputStream fuera = new FileOutputStream(file);
+            bitmap1.compress(Bitmap.CompressFormat.PNG,100,fuera);
+            fuera.flush();
+            fuera.close();
+            file.setReadable(true, false);
+            final Intent comparte = new Intent(android.content.Intent.ACTION_SEND);
+            comparte.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+            comparte.setType("*/*");
+            comparte.putExtra(Intent.EXTRA_STREAM, Uri.fromFile(file));
+            comparte.putExtra(Intent.EXTRA_TEXT, "Nombre:" + nombres + "\n Carrera:" + carrera1 + "\n" + twitter1 + "\n" + github1 + "\n" + correoe + "\n" + tel);
+            startActivity(Intent.createChooser(comparte,"Que programa desea utilizar?"));
+
+        } catch (Exception e) {
+            e.printStackTrace();
+
+
         }
-
     }
 
+    private Bitmap vista(View view){
+        Bitmap retorna = Bitmap.createBitmap(view.getWidth(), view.getHeight(), Bitmap.Config.ARGB_8888);
+        Canvas canvas1 = new Canvas(retorna);
+        Drawable imag = view.getBackground();
 
+        if(imag !=null){
+            imag.draw(canvas1);
+        }else{
+            canvas1.drawColor(Color.WHITE);
 
+        }
+        view.draw(canvas1);
+        return retorna;
+    }
 }
